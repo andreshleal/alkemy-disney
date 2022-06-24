c3e1ff1e798399f3a579a93200946e7a66cefc9c
homeristico <homeristico@hotmail.com>
2022-06-23 21:55:38 -0500

    filter methods PeliculaSerieRepository

--- src/main/java/com/alkemy/disney/repositories/PeliculaSerieRepository.java
+++ src/main/java/com/alkemy/disney/repositories/PeliculaSerieRepository.java
@@ -1,10 +1,22 @@
 package com.alkemy.disney.repositories;
 
 import com.alkemy.disney.entities.PeliculaSerie;
+import com.alkemy.disney.entities.Personaje;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PeliculaSerieRepository extends JpaRepository<PeliculaSerie, Long> {
+
+    Page<PeliculaSerie> findPeliculaSeriesByTitulo(String titulo, Pageable pageable);
+
+    Page<PeliculaSerie> findPeliculaSeriesByPersonaje(Personaje personaje, Pageable pageable);
+
+    Page<PeliculaSerie> findPeliculaSeriesByTituloAndPersonaje(
+            String titulo, Personaje personaje, Pageable pageable
+    );
+
 
 }
